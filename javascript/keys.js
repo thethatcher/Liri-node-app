69cbb1edac193f3f69e65e11e360a92822eee2c4
thethatcher <thethatcher@gmail.com>
2017-08-22 17:33:22 -0600

    basic functionality of the keys present.

---
+++ javascript/keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '11sK5c07ps9D1gnUoGAup9VXK',
+  consumer_secret: 'SRbep2Ly1lkg66Mj7Ygbgd0Xu6TORXQ4uRcQ7Uyh1xo41lBeuT',
+  access_token_key: '899815959577231362-ZLx6JuZdrGcBLoUJ5NzIks6quP32uQB',
+  access_token_secret: '6e8xJEjQeLz3y1TSA5JHZbUjzGVB3ivIHszD9Vb4j8A8P',
+}
---
+++ javascript/liri.js
@@ -0,0 +1,4 @@
+var fs = require("fs");
+var keys = require('./keys.js');
+
+console.log(keys.twitterKeys);
