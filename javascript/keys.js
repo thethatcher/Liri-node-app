fa74bd9e4a97011a7e64288107943988c06aed4f
thethatcher <thethatcher@gmail.com>
2017-08-24 19:59:46 -0600

    twitter and spotify functions working

--- javascript/keys.js
+++ javascript/keys.js
@@ -1,8 +1,11 @@
-console.log('this is loaded');
-
 exports.twitterKeys = {
   consumer_key: '11sK5c07ps9D1gnUoGAup9VXK',
   consumer_secret: 'SRbep2Ly1lkg66Mj7Ygbgd0Xu6TORXQ4uRcQ7Uyh1xo41lBeuT',
   access_token_key: '899815959577231362-ZLx6JuZdrGcBLoUJ5NzIks6quP32uQB',
   access_token_secret: '6e8xJEjQeLz3y1TSA5JHZbUjzGVB3ivIHszD9Vb4j8A8P',
+}
+
+exports.spotifyKeys = {
+	id:"e88f4c8c4c8a4c5cadb9bf6b2a51f902"
+	,secret:"6de0c57f99504163b62a731f6ec74b38"
 }
--- javascript/liri.js
+++ javascript/liri.js
@@ -1,4 +1,50 @@
+var inquire = require("inquirer");
 var fs = require("fs");
+var twitter = require("twitter");
 var keys = require('./keys.js');
+var spotify = require('node-spotify-api');
+var client = new twitter({
+	consumer_key: keys.twitterKeys.consumer_key
+	,consumer_secret: keys.twitterKeys.consumer_secret
+	,access_token_key: keys.twitterKeys.access_token_key
+	,access_token_secret: keys.twitterKeys.access_token_secret
+});
+var spotifyClient = new spotify({
+	 id: keys.spotifyKeys.id
+	,secret: keys.spotifyKeys.secret
+}); 
 
-console.log(keys.twitterKeys);
+spotifyThisSong("the impression that I get");
+
+function myTweets(){
+	client.get("statuses/user_timeline",{screen_name:"bootcamptrash"}, function(error, tweets, response){
+		if(error){console.log(error);}
+		else{
+			for (var i = 0; i < 20; i++) {
+				if(i < tweets.length){
+					console.log(tweets[i].created_at);
+					console.log(tweets[i].text + "\n-------------------");
+				}
+			}
+		}
+	});
+}
+
+function spotifyThisSong(song){
+	spotifyClient.search({ type:"track", query: song, limit:"1"},function(err, data){
+		console.log("Artist: " + data.tracks.items[0].artists[0].name);
+		console.log("Album: " + data.tracks.items[0].album.name);
+		console.log("Song: " + data.tracks.items[0].name);
+		console.log("Preview: " + data.tracks.items[0].preview_url);
+	})
+}
+
+function movieThis(movie){
+
+}
+
+function doWhatItSays(){
+	fs.read("../random.txt","utf8",function(error,data){
+
+	});
+}
